f40ff97b2d73f0092f9f0f0393ca433aa72ba2c0
Filippo Vissani <filippo.vissani.99@gmail.com>
2026-01-11 11:24:09 +0100

    ci: refactor CI workflows to use GraalVM and modularize checks, tests, and releases

--- release.config.js
+++ release.config.js
@@ -1,8 +1,7 @@
 const config = require('semantic-release-preconfigured-conventional-commits')
 const publishCommands = `
-./gradlew --no-daemon clean build || exit 1
-git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md || exit 2
-git push --force origin \${nextRelease.version} || exit 3
+git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md || exit 1
+git push --force origin \${nextRelease.version} || exit 2
 `
 const releaseBranches = ["main"]
 config.branches = releaseBranches
@@ -13,7 +12,10 @@ config.plugins.push(
     }],
     ["@semantic-release/github", {
         "assets": [
-            { "path": "build/libs/*.jar" }
+            { "path": "build/libs/*.jar" },
+            { "path": "build/native/nativeCompile/portfolium-linux-amd64" },
+            { "path": "build/native/nativeCompile/portfolium-macos-amd64" },
+            { "path": "build/native/nativeCompile/portfolium-windows-amd64.exe" }
         ]
     }],
     ["@semantic-release/git", {
